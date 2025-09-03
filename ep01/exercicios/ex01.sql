b9d8e3c91ab15d308340be5e4b81a62b4e2c1883
adriacode <adriacoding@gmail.com>
2025-09-03 15:03:17 -0300

    feat: primeiras queries

--- ep01/exercicios/ex01.sql
+++
@@ -1,5 +0,0 @@
--- Ex1: Quantos produtos temos da categoria 'artes'?
-SELECT count(product_id) as "Quantidade",
-       product_category_name
-FROM tb_products
-WHERE product_category_name = 'artes'
--- ep01/exercicios/ex02.sql
+++
@@ -1,7 +0,0 @@
-
--- Ex2: Quantos produtos tem mais de 5 litros?
-SELECT count(DISTINCT product_id) as "Quantidade"
-
-FROM tb_products
-WHERE (product_height_cm * product_length_cm * product_width_cm) / 1000 > 5
-
